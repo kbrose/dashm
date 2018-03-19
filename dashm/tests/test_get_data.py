65f1680319bf7f3323e9c2c166e608678ae61b37
Kevin Rose <kevin+gh@maypark.com>
2018-03-19 00:30:27 -0500

    Test get_data's CLI

--- dashm/tests/test_get_data.py
+++ dashm/tests/test_get_data.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 from pathlib import Path
 import shutil
 
@@ -10,6 +11,7 @@ from dashm.data import get_data
 class Test_Clone():
     @classmethod
     def setup_method(cls):
+        old_sys_argv = sys.argv
         cls.data_path = Path(__file__).parents[2] / 'data/'
         for interim in ['raw-repos', 'processed-repos']:
             dst = cls.data_path / interim / 'dashm-testing'
@@ -21,6 +23,7 @@ class Test_Clone():
                 os.remove(str(dst) + '.dashm')
             except FileNotFoundError:
                 pass
+        sys.argv = old_sys_argv
 
     teardown_method = setup_method
 
@@ -29,6 +32,14 @@ class Test_Clone():
         dst = Path(__file__).parents[2] / 'data/raw-repos/dashm-testing'
         assert os.path.exists(dst)
         assert os.path.exists(str(dst) + '.dashm')
+
+    def test_cli(self):
+        sys.argv = ['python', 'https://github.com/kbrose/dashm-testing.git']
+        get_data.cli()
+        dst = Path(__file__).parents[2] / 'data/raw-repos/dashm-testing'
+        assert os.path.exists(dst)
+        assert os.path.exists(str(dst) + '.dashm')
+
 
 
 class Test_CLI():
